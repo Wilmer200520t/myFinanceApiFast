51d152bf2bf7ecdf48ee7306f04a86f6b8e09a07
Wilmer Zuñiga <wilmer.franco.65@gmail.com>
2024-07-18 13:08:21 -0500

    feature: Add routes and conection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mysql from "../db/index.js";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 const db = mysql;
 app.use(express.json());
+app.use(cors());
 
 app.get("/sql", (req, res) => {
   const { query } = req.body;
